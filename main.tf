4190f69b69678fa90fac6b2af5b8aa779899cbda
Lennart <lp@propassig.de>
2022-06-30 18:58:58 +0200

    New :)

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "myterraformnetwork" {
-  name                = "myVnet"
+  name                = "${var.NAME}-Vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -31,7 +31,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 
 # Create subnet
 resource "azurerm_subnet" "myterraformsubnet" {
-  name                 = "project-${var.NAME}"
+  name                 = "${var.NAME}-Subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -40,7 +40,7 @@ resource "azurerm_subnet" "myterraformsubnet" {
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
-  name                = "NSG-${var.NAME}"
+  name                = "${var.NAME}-NSG"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
