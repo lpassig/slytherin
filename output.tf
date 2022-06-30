332170f2373517aea9789a88c81a94606bf55cac
Lennart <lp@propassig.de>
2022-06-30 20:24:51 +0200

    Changes

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "vnet_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_virtual_network.myterraformnetwork.name
   sensitive = false
 }
 
-output "subnet_name" {
-  value = azurerm_virtual_network.myterraformnetwork.name
+output "subnet_id" {
+  value = azurerm_subnet.myterraformsubnet.id
   sensitive = false
 }
 
